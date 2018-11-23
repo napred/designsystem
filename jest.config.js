a956751e80fdaf08663451ef62733340b42404b2
Michal Kvasničák <michal.kvasnicak@gmail.com>
2018-11-23 18:30:57 +0100

    generate normal css classes in snapshots

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   preset: 'jest-puppeteer',
   setupTestFrameworkScriptFile: './jest/setup.js',
-  snapshotSerializers: ['enzyme-to-json/serializer', 'jest-emotion/serializer'],
+  snapshotSerializers: ['enzyme-to-json/serializer'],
   collectCoverageFrom: ['packages/**/src/**/*.{js,jsx}'],
   transform: {
     '^.+\\.jsx?$': './jest/transformJs.js',
--- jest/setup.js
+++ jest/setup.js
@@ -2,6 +2,8 @@
 /* eslint-disable import/no-extraneous-dependencies, node/no-unpublished-require */
 
 import 'react-testing-library/cleanup-after-each';
+import * as emotion from 'emotion';
+import { createSerializer } from 'jest-emotion';
 
 const Adapter = require('enzyme-adapter-react-16');
 const { configure } = require('enzyme');
@@ -20,3 +22,11 @@ const toMatchImageSnapshot = configureToMatchImageSnapshot({
 });
 
 expect.extend({ toMatchImageSnapshot });
+
+expect.addSnapshotSerializer(
+  createSerializer(emotion, {
+    classNameReplacer(className) {
+      return className;
+    },
+  }),
+);
