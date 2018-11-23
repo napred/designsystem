059cb3759a54479a6c9f5bcc27aec9d01a36f1e1
Michal Kvasničák <michal.kvasnicak@gmail.com>
2018-11-23 18:30:39 +0100

    export everything from package

--- packages/ds/src/index.js
+++ packages/ds/src/index.js
@@ -1,10 +1,13 @@
 // @flow
 
-import DesignSystemContext from './context';
-
-export { DesignSystemContext };
+export * from './cache';
 export * from './hooks';
+export * from './system';
+export * from './theme';
+export * from './utilities';
+export * from './styles';
 export { default as Box } from './Box';
 export { default as DesignSystem } from './DesignSystem';
+export { default as DesignSystemContext } from './context';
 export { default as IfViewport } from './IfViewport';
 export { default as createComponent } from './createComponent';
