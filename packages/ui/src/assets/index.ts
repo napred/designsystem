072618b4abdcf62190c5e4d25e2d7feebb000440
jurajhrib <ancientpedestrian@gmail.com>
2018-12-04 18:50:36 +0100

    fixed svg icon

--- packages/ui/src/assets/index.ts
+++ packages/ui/src/assets/index.ts
@@ -1,4 +1,3 @@
-import { createComponent } from '@napred/ds';
 import closeIcon from '../../assets/close.svg';
 
-export const CloseIcon = createComponent('CloseIcon', closeIcon);
+export const CloseIcon = closeIcon;
