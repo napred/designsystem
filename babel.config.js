22d3d76a078af82ce37bc446a8fcea5e392527ad
Michal Kvasničák <michal.kvasnicak@gmail.com>
2018-12-07 18:18:07 +0100

    add jest projects

--- babel.config.js
+++
@@ -1,4 +0,0 @@
-const { readFileSync } = require('fs');
-const { resolve } = require('path');
-
-module.exports = JSON.parse(readFileSync(resolve(__dirname, './.storybook/.babelrc'), 'utf8'));
--- jest.config.js
+++ jest.config.js
@@ -1,25 +1,8 @@
 module.exports = {
-  /* globals: {
-    'ts-jest': {
-      diagnostics: true,
-    },
-  },
-  preset: 'jest-puppeteer',
-  setupTestFrameworkScriptFile: './jest/setup.js',
-  snapshotSerializers: ['enzyme-to-json/serializer'],*/
-  // collectCoverageFrom: ['packages/**/src/**/*.{js,jsx,ts,tsx}'],
-  // moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
-  /* moduleNameMapper: {
-    '@napred/ds': '<rootDir>/packages/ds/src',
-    '@napred/primitives': '<rootDir>/packages/primitives/src',
-    '@napred/ui': '<rootDir>/packages/ui/src',
-    '\\.svg': '<rootDir>/jest/transformSvg.js',
-  },*/
-  projects: ['<rootDir>/packages/*'],
-  /* testEnvironment: 'jsdom',
-  testRegex: '(/test/(?!test-utils\b)\b.*|\\.(test|spec))\\.(ts|tsx|js|jsx)$',
-  transform: {
-    '^.+\\.jsx?$': './jest/transformJs.js',
-    '^.+\\.tsx?$': 'ts-jest',
-  },*/
+  projects: [
+    '<rootDir>/packages/ds/jest.config.js',
+    '<rootDir>/packages/native/jest.config.js',
+    '<rootDir>/packages/primitives/jest.config.js',
+    '<rootDir>/packages/ui/jest.config.js',
+  ],
 };
