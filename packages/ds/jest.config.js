8e55b349387c82b7df090701860ca2bc6ef3eaee
Michal Kvasničák <michal.kvasnicak@gmail.com>
2018-12-07 18:16:14 +0100

    add jest configuration for ds

--- packages/ds/jest.config.js
+++ packages/ds/jest.config.js
@@ -1,24 +1,18 @@
 module.exports = {
   displayName: '@napred/ds',
-  rootDir: './../..',
+  rootDir: __dirname,
   globals: {
     'ts-jest': {
       diagnostics: true,
     },
   },
   setupTestFrameworkScriptFile: '<rootDir>/jest/setup.js',
-  collectCoverageFrom: ['packages/**/src/**/*.{js,jsx,ts,tsx}'],
+  collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
-  moduleNameMapper: {
-    '@napred/ds': '<rootDir>/packages/ds/src',
-    '@napred/primitives': '<rootDir>/packages/primitives/src',
-    '@napred/ui': '<rootDir>/packages/ui/src',
-    '\\.svg': '<rootDir>/jest/transformSvg.js',
-  },
   testEnvironment: 'jsdom',
-  testMatch: ['<rootDir>/packages/ds/**/__tests__/*.(test|spec).{js,jsx,ts,tsx}'],
+  testMatch: ['<rootDir>/**/__tests__/*.(test|spec).{js,jsx,ts,tsx}'],
   transform: {
-    '^.+\\.jsx?$': './jest/transformJs.js',
-    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.(js|jsx)$': 'babel-jest',
+    '^.+\\.(ts|tsx)$': 'ts-jest',
   },
 };
