856404bda4426400f08358c5cfa86dd7f3e876fa
Michal Kvasničák <michal.kvasnicak@gmail.com>
2018-12-09 16:43:09 +0100

    remove jest-emotion from ds jest setup

--- packages/ds/jest/setup.js
+++ packages/ds/jest/setup.js
@@ -1,14 +1,3 @@
 require('react-testing-library/cleanup-after-each');
 
-const emotion = require('emotion');
-const { createSerializer } = require('jest-emotion');
-
 jest.setTimeout(30 * 1000);
-
-expect.addSnapshotSerializer(
-  createSerializer(emotion, {
-    classNameReplacer(className) {
-      return className;
-    },
-  }),
-);
