1b8c4b6f918be2e56d7bbd73ca119965d92cc9ac
Michal Kvasničák <michal.kvasnicak@gmail.com>
2018-12-09 16:36:29 +0100

    simplify style factories

--- packages/ds/src/styles/systemStyleFactories.ts
+++ packages/ds/src/styles/systemStyleFactories.ts
@@ -1,82 +1,60 @@
 import { ISystem } from '../system';
 import { IStyler } from '../types';
-import convertUnit from '../utilities/convertUnit';
+import { arrayize, convertUnit, getResponsiveValue } from '../utilities';
 import formatStyleResult from './formatStyleResult';
 
-export function createSystemStyle<T extends object>(
-  propName: string,
+export function createSystemStyle<TProps extends object>(
+  propName: keyof TProps,
   cssAttribute: string | string[],
   systemName: string,
-  formatter: (value: number | string) => number | string,
+  formatter: (value: number | string) => string | number,
   defaultValue?: string | number | Array<string | number> | void,
-): IStyler<T> {
-  const cssAttributes: string[] = Array.isArray(cssAttribute) ? cssAttribute : [cssAttribute];
+): IStyler<TProps> {
+  const cssAttributes: string[] = arrayize(cssAttribute);
+  const defaultValues = arrayize(defaultValue || []);
 
   return {
-    apply: (props: { [key: string]: any }, { theme, viewport: bp }: ISystem) => {
-      const propValue = props[propName] == null ? defaultValue : props[propName];
+    apply: (props: TProps, { theme, viewport: bp }: ISystem) => {
+      const propValue = arrayize<string | number | null | undefined>(props[propName] as any);
       const systemValue = theme.get(systemName);
 
-      if (propValue == null) {
+      const bpValue = getResponsiveValue(bp, propValue, defaultValues);
+
+      if (bpValue == null) {
         return formatStyleResult(null, cssAttributes, formatter);
       }
 
-      const bpValue = Array.isArray(propValue) ? propValue[bp] : propValue;
+      const bpValueNum = Number(bpValue);
+      const isNegativeInt =
+        !Number.isNaN(bpValueNum) && Number.isInteger(bpValueNum) && bpValueNum < 0;
 
-      if (bpValue != null) {
-        const isNegativeInt = Number.isInteger(bpValue) && bpValue < 0;
+      const systemBpValue = systemValue[isNegativeInt ? -bpValue : bpValue] || bpValue;
 
-        const systemBpValue = systemValue[isNegativeInt ? -bpValue : bpValue] || bpValue;
-
-        return formatStyleResult(
-          isNegativeInt ? -Number(systemBpValue) : systemBpValue,
-          cssAttributes,
-          formatter,
-        );
-      }
-
-      // if there is no value for given breakpoint
-      // look if we have default value
-      // if we don't have default value, return empty object
-      // if  default value is scalar return it
-      // if default value is responsive:
-      // - return value for given breakpoint
-      // - or return the last value from longer of those
-      if (Array.isArray(defaultValue)) {
-        // if propValue is responsive, choose the longer array and select last value
-        const arr = propValue.length >= defaultValue.length ? propValue : defaultValue;
-        // tslint:disable-next-line:no-shadowed-variable
-        const val = arr[arr.length - 1];
-
-        return formatStyleResult(systemValue[val] || val, cssAttributes, formatter);
-      }
-
-      // default value is scalar
-      // in this case return the last value from propValue if is array
-      const val = propValue[propValue.length - 1];
-      const systemVal = systemValue[val] || val;
-
-      return formatStyleResult(systemVal, cssAttributes, formatter);
+      return formatStyleResult(
+        isNegativeInt ? -Number(systemBpValue) : systemBpValue,
+        cssAttributes,
+        formatter,
+      );
     },
     propNames: [propName],
     stripProps: [propName],
   };
 }
 
-export function createNumericSystemStyle<T extends object>(
-  propName: string,
+export function createNumericSystemStyle<TProps extends object>(
+  propName: keyof TProps,
   cssAttribute: string | string[],
   systemName: string,
   defaultValue?: number | string | Array<number | string> | void,
-): IStyler<T> {
+): IStyler<TProps> {
   return createSystemStyle(propName, cssAttribute, systemName, convertUnit, defaultValue);
 }
 
-export function createStringSystemStyle<T extends object>(
-  propName: string,
+export function createStringSystemStyle<TProps extends object>(
+  propName: keyof TProps,
   cssAttribute: string | string[],
   systemName: string,
   defaultValue?: string | string[] | void,
-): IStyler<T> {
+): IStyler<TProps> {
   return createSystemStyle(propName, cssAttribute, systemName, val => val, defaultValue);
 }
