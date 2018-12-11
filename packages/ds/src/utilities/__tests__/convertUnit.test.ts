0770e8e5950658feda641e00b23d819d45bcb72a
Michal Kvasničák <michal.kvasnicak@gmail.com>
2018-12-11 13:28:16 +0100

    make convertUnit more intelligent and allow to multiply values

--- packages/ds/src/utilities/__tests__/convertUnit.test.ts
+++ packages/ds/src/utilities/__tests__/convertUnit.test.ts
@@ -17,7 +17,34 @@ describe('mixins/convertUnit', () => {
 
   it('passes through exact units', () => {
     expect(convert('10px')).toBe('10px');
+    expect(convert('-10px')).toBe('-10px');
     expect(convert('100%')).toBe('100%');
+    expect(convert('-100%')).toBe('-100%');
     expect(convert('1rem')).toBe('1rem');
+    expect(convert('-1rem')).toBe('-1rem');
+  });
+
+  it('allows to multiply values', () => {
+    expect(convert('10', 2)).toBe('20px');
+    expect(convert('0', 2)).toBe('0px');
+    expect(convert(0, 2)).toBe('0px');
+    expect(convert(10, 2)).toBe('20px');
+    expect(convert(-10, 2)).toBe('-20px');
+    expect(convert(1 / 3, 2)).toBe('66.66666666666666%');
+    expect(convert(1 / 2, 2)).toBe('100%');
+    expect(convert(-2 / 4, 2)).toBe('-100%');
+    expect(convert('10px', 2)).toBe('20px');
+    expect(convert('-10px', 2)).toBe('-20px');
+    expect(convert('-10 px', 2)).toBe('-20px');
+    expect(convert('100%', 2)).toBe('200%');
+    expect(convert('-100%', 2)).toBe('-200%');
+    expect(convert('-100 %', 2)).toBe('-200%');
+    expect(convert('-33.33%', 2)).toBe('-66.66%');
+    expect(convert('-33.33 %', 2)).toBe('-66.66%');
+    expect(convert('1rem', 2)).toBe('2rem');
+    expect(convert('-1rem', 2)).toBe('-2rem');
+    expect(convert('-1 rem', 2)).toBe('-2rem');
+    expect(convert('-1.5rem', 2)).toBe('-3rem');
+    expect(convert('-1.5 rem', 2)).toBe('-3rem');
   });
 });
