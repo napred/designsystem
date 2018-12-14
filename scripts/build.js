ed0a5987f25dfedcbb83d17872aa85dfa2fc11b9
Michal Kvasničák <michal.kvasnicak@gmail.com>
2018-12-14 10:35:37 +0100

    get rid of warning during build

--- scripts/build.js
+++ scripts/build.js
@@ -3,16 +3,16 @@ const shell = require('shelljs');
 console.log(process.cwd());
 
 shell.cd('packages/ds');
-shell.exec('npm run build');
+shell.exec('npm run build --scripts-prepend-node-path');
 
 shell.cd('../browser');
-shell.exec('npm run build');
+shell.exec('npm run build --scripts-prepend-node-path');
 
 shell.cd('../native');
-shell.exec('npm run build');
+shell.exec('npm run build --scripts-prepend-node-path');
 
 shell.cd('../primitives');
-shell.exec('npm run build');
+shell.exec('npm run build --scripts-prepend-node-path');
 
 shell.cd('../ui');
-shell.exec('npm run build');
+shell.exec('npm run build --scripts-prepend-node-path');
