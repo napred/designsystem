023c8ff9b2ea3898c4fb97ee4e1c912bad69c569
Michal Kvasničák <michal.kvasnicak@gmail.com>
2018-12-17 12:09:26 +0100

    do not hoist uniprimitives

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,9 @@
 module.exports = {
   // hack because jest does not support global* in project configs
   // https://github.com/facebook/jest/issues/5441
-  globalSetup: 'jest-environment-puppeteer/setup',
-  globalTeardown: 'jest-environment-puppeteer/teardown',
+  globalSetup: '<rootDir>/packages/uniprimitives/node_modules/jest-environment-puppeteer/setup',
+  globalTeardown:
+    '<rootDir>/packages/uniprimitives/node_modules/jest-environment-puppeteer/teardown',
   projects: [
     '<rootDir>/packages/browser/jest.config.js',
     '<rootDir>/packages/ds/jest.config.js',
