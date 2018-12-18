1adc9b063d1ff19897af9c87837819d2131b5227
Michal Kvasničák <michal.kvasnicak@gmail.com>
2018-12-18 17:11:58 +0100

    move all babel devDependnecies from native package to root

--- packages/native/jest.config.js
+++ packages/native/jest.config.js
@@ -9,18 +9,28 @@ module.exports = {
       diagnostics: true,
     },
   },
-  preset: 'react-native',
+  haste: {
+    defaultPlatform: 'ios',
+    platforms: ['android', 'ios', 'native'],
+    hasteImplModulePath: '<rootDir>/../../node_modules/react-native/jest/hasteImpl.js',
+    providesModuleNodeModules: ['react-native'],
+  },
   rootDir: __dirname,
+  modulePathIgnorePatterns: ['<rootDir>/../../node_modules/react-native/Libraries/react-native/'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
   moduleNameMapper: {
     '@napred/ds': resolve(__dirname, '../ds/src'),
     '^react$': resolve(__dirname, '../../node_modules/react'),
     '^React$': resolve(__dirname, '../../node_modules/react'),
-    '\\.svg': '<rootDir>/jest/transformSvg.js',
+    '^.+\\.(bmp|gif|jpg|jpeg|mp4|png|psd|svg|webp)$':
+      '<rootDir>/../../node_modules/react-native/jest/assetFileTransformer.js',
   },
   testMatch: ['<rootDir>/**/__tests__/*.(test|spec).{js,jsx,ts,tsx}'],
   transform: {
     '\\.tsx?$': 'ts-jest',
-    '\\.js$': '<rootDir>/node_modules/react-native/jest/preprocessor.js',
+    '\\.js$': '<rootDir>/../../node_modules/react-native/jest/preprocessor.js',
   },
+  transformIgnorePatterns: ['node_modules/(?!(jest-)?react-native|react-clone-referenced-element)'],
+  setupFiles: ['<rootDir>/../../node_modules/react-native/jest/setup.js'],
+  testEnvironment: 'node',
 };
